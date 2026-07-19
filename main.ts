6080e5611d445039b2d5a861556561d4edcc1a8f
asag22 <letypdiwad@gmail.com>
2026-07-19 21:43:07 +0200

    initial

--- main.ts
+++ main.ts
@@ -1,4 +1,13 @@
 // deno-lint-ignore-file no-import-prefix
-import { add } from "./add.ts";
+import { parse, resolve } from "jsr:@std/path@1.1.6";
 
-console.log(`4+3=${add(4,3)}`);
+const arg = Deno.args[0] ?? "";
+const parsed = parse(arg);
+
+if([".zip", ".rar", ".7z"].includes(parsed.ext)){
+    const newFolder = resolve(parsed.dir, parsed.name);
+    new Deno.Command("7z", { args: [ "x", `-o${newFolder}`, arg ] }).spawn();
+}
+
+
+console.log("file should be .zip, .rar or .7z");
