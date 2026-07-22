6019a7c9adad51ea4282457d80597ad248c34981
Dawid Pytel <letypdiwad@gmail.com>
2026-07-22 03:47:20 +0200

    testing how path resolves symlinks

--- main.ts
+++ main.ts
@@ -1,11 +1,18 @@
 // deno-lint-ignore-file no-import-prefix ban-unused-ignore
-import { parse, resolve } from "jsr:@std/path@1.1.6";
+// import { parse, resolve } from "jsr:@std/path@1.1.6";
+import * as path from "jsr:@std/path@1.1.6";
 
 const arg = Deno.args[0] ?? "";
-const parsed = parse(arg);
+const parsed = path.parse(arg);
+
+let storage = path.parse("~/storage/downloads");
+
+console.log(storage.dir);
+console.log(path.resolve(storage.dir));
+
 
 if([".zip", ".rar", ".7z"].includes(parsed.ext)){
-    const newFolder = resolve(parsed.dir, parsed.name);
+    const newFolder = path.resolve(parsed.dir, parsed.name);
     new Deno.Command("7z", { args: [ "x", `-o${newFolder}`, arg ] }).spawn();
 }
 
